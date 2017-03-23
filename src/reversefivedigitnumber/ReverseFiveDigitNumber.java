ce9ec628aa63fb9fb827edc55dacbcd6b37ccd41
Beenish Sajjad <beenishsajjad277@gmail.com>
2017-03-23 14:52:38 -0700

    Changes by Umer

--- src/reversefivedigitnumber/ReverseFiveDigitNumber.java
+++ src/reversefivedigitnumber/ReverseFiveDigitNumber.java
@@ -11,23 +11,7 @@ public class ReverseFiveDigitNumber {
         
         int number1,number2,number3,number4,number5,number6,number7,number8,n;
         
-        Scanner bunny=new Scanner(System.in);
-        System.out.println("enter the 3-digit number:");
-        n=bunny.nextInt();
-        
-        number1=n/10000;
-        n=n%10000;
-        
-        number3=n/1000;
-        n=n%1000;
-        
-        number5=n/100;
-        n=n%100;
-        
-        number7=n/10;
-        n=n%10;
-        
-        System.out.println("Reversed number is:"+n+number7+number5+number3+number1);
+        System.out.println("I have changed this project");
     }
     
 }
